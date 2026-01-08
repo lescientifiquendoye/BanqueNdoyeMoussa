c24c21bc88cfa72edd2ce2a3fe6bc82c02286a7d
Moussa Ndoye <moussandoye@esp.sn>
2026-01-08 21:25:41 +0000

    first commit

---
+++ BanqueNdoyeMoussa/CompteBancaire.cs
@@ -0,0 +1,64 @@
+﻿namespace Banque
+{
+    public class CompteBancaire
+    {
+        private readonly string _nomClient;
+        private bool _bloque = false;
+
+        private CompteBancaire() { }
+
+        public CompteBancaire(string nomClient, double solde)
+        {
+            _nomClient = nomClient;
+            Solde = solde;
+        }
+
+        public double Solde { get; private set; }
+
+        public void Débiter(double montant)
+        {
+            if (_bloque)
+            {
+                throw new Exception("Compte bloqué");
+            }
+
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(montant, other: Solde);
+
+            if (montant < 0)
+            {
+                throw new ApplicationException("Le montant retiré doit être Positif");
+            }
+
+            Solde -= montant; // code intentionnellement faux
+        }
+
+        public void Créditer(double montant)
+        {
+            if (_bloque)
+            {
+                throw new Exception("Compte bloqué");
+            }
+            
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(montant);
+            Solde += montant;
+        }
+
+        private void BloquerCompte()
+        {
+            _bloque = true;
+        }
+
+        private void DébloquerCompte()
+        {
+            _bloque = false;
+        }
+
+        public static void Main()
+        {
+            var cb = new CompteBancaire(nomClient: "Pr Mamadou Samba Camara", solde: 500000);
+            cb.Créditer(montant: 100000);
+            cb.Débiter(montant: 50000);
+            Console.WriteLine($"Solde disponible : {cb.Solde}");
+        }
+    }
+}
---
+++ BanqueTests/CompteBancaireTests.cs
@@ -0,0 +1,38 @@
+﻿using Banque;
+namespace BanqueTests;
+
+[TestClass]
+public sealed class CompteBancaireTests
+{
+    [TestMethod]
+    public void VérifierDébitCompteCorrect()
+    {
+        // Arrange
+        const double soldeInitial = 500000;
+        const double montantDébit = 400000;
+        const double soldeAttendu = 100000;
+        var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+        // Act
+        cb.Débiter(montantDébit);
+
+        // Assert
+        Assert.AreEqual(soldeAttendu, cb.Solde, delta: 0.001, message: "Compte débité incorrectement");
+    }
+
+    [TestMethod()]
+    public void DébiterMontantNégatifSoulèveApplicationException()
+    {
+        //Arrange
+        const double soldeInitial = 500000;
+        const double montantDébit = -400000;
+        var cb = new CompteBancaire(nomClient: "Pr Ibrahima N6om", soldeInitial);
+
+        // Act
+        void Action() => cb.Débiter(montantDébit);
+
+        // Assert
+        var ex = Assert.Throws<ApplicationException>(Action);
+        Assert.AreEqual("Le montant retiré doit être Positif", ex.Message);
+    }
+}
